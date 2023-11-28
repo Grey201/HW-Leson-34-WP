991a99335e9b4c0de9dec7c5849240f1553861e3
Grey <serg3@yandex.ru>
2023-11-28 22:38:21 +0300

    assembly done

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+// .eslintrc.js example
+export default {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": ["prettier", "eslint:recommended"],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+  }
